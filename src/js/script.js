c3131ba4703ab39413c659d6ed03e520196d3e71
Samuel Reutcky <sreutcky@gmail.com>
2024-11-27 10:02:17 -0600

    Added name animation

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,19 @@ function select(selector, scope = document) {
 function listen(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
+
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+  let paths = document.querySelectorAll("path");
+  let mode=repeat?'infinite':'forwards'
+  for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+      const length = path.getTotalLength();
+      path.style["stroke-dashoffset"] = `${length}px`;
+      path.style["stroke-dasharray"] = `${length}px`;
+      path.style["stroke-width"] = `${strokeWidth}px`;
+      path.style["stroke"] = `${strokeColor}`;
+      path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+      path.style["animation-delay"] = `${i * delay}s`;
+  }
+}
+setTextAnimation(0.1, 3.3, 2, 'ease-in-out', '#ffffff', false);
